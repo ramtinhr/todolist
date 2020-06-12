86cd13d6f7578644a4fd738323017cea2a179023
ramtin <ramtin.hrezaei@gmail.com>
2020-06-12 11:41:10 +0430

    Add gitignore,Procfile , update parts

--- app.js
+++ app.js
@@ -150,5 +150,8 @@ app.post('/delete',(req,res) => {
 });
 
 
-
-app.listen(3000,()=>console.log('server is runnig on port 3000'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+  }
+app.listen(port,()=>console.log('server started successfully'));
